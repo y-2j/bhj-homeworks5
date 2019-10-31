edb3527fa84bd03fd280beca67e228ec7e5bdc3e
y-2j <etozhedafomenko@gmail.com>
2019-10-31 10:28:20 +0400

    Задача с выпадающим списком

--- autocomplete/task.js
+++ autocomplete/task.js
@@ -68,25 +68,19 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    const optionsList = this.input.options;
+    const matches = [];
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    for (let i = 0; i < optionsList.length; i++) {
+      if (optionsList[i].text.includes(text)) {
+        matches.push({
+          text: optionsList[i].text,
+          value: optionsList[i].value
+        });
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+    
+    return matches;
   }
 }
 
